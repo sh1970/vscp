2c2d4adfe6a99c7898a6c1d6abe10fe129e435fc
Ake Hedman <akhe@grodansparadis.com>
2026-03-13 11:51:51 +0100

    Fixed mosquitto search path for windows workflow

--- cmake/modules/FindMosquitto.cmake
+++ cmake/modules/FindMosquitto.cmake
@@ -6,25 +6,16 @@
 #  MOSQUITTO_LIBRARIES    - List of libraries when using libmosquitto.
 #  MOSQUITTO_FOUND        - True if libmosquitto found.
 
-# Build vcpkg hint paths
-if(DEFINED VCPKG_INSTALLED_DIR AND DEFINED VCPKG_TARGET_TRIPLET)
-  set(_VCPKG_INCLUDE_DIR "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include")
-  set(_VCPKG_LIB_DIR "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib")
-elseif(DEFINED ENV{VCPKG_ROOT} AND DEFINED VCPKG_TARGET_TRIPLET)
-  set(_VCPKG_INCLUDE_DIR "$ENV{VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/include")
-  set(_VCPKG_LIB_DIR "$ENV{VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/lib")
-endif()
-
 if (NOT MOSQUITTO_INCLUDE_DIR)
   find_path(MOSQUITTO_INCLUDE_DIR mosquitto.h
-    HINTS
-      # Windows / vcpkg
-      ${_VCPKG_INCLUDE_DIR}
+    PATHS
+      ${CMAKE_PREFIX_PATH}
       # macOS / Homebrew
-      /opt/homebrew/include
-      /usr/local/include
-      /opt/homebrew/opt/mosquitto/include
-      /usr/local/opt/mosquitto/include
+      /opt/homebrew
+      /usr/local
+      /opt/homebrew/opt/mosquitto
+      /usr/local/opt/mosquitto
+    PATH_SUFFIXES include
   )
 endif()
 
@@ -32,14 +23,14 @@ if (NOT MOSQUITTO_LIBRARY)
   find_library(
     MOSQUITTO_LIBRARY
     NAMES mosquitto
-    HINTS
-      # Windows / vcpkg
-      ${_VCPKG_LIB_DIR}
+    PATHS
+      ${CMAKE_PREFIX_PATH}
       # macOS / Homebrew
-      /opt/homebrew/lib
-      /usr/local/lib
-      /opt/homebrew/opt/mosquitto/lib
-      /usr/local/opt/mosquitto/lib
+      /opt/homebrew
+      /usr/local
+      /opt/homebrew/opt/mosquitto
+      /usr/local/opt/mosquitto
+    PATH_SUFFIXES lib
   )
 endif()
 
