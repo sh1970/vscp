265cbd15893269945b3981e71dd26b5846e31b45
Ake Hedman <akhe@grodansparadis.com>
2026-03-13 12:25:50 +0100

    Fixed linux workflow

--- cmake/modules/FindMosquitto.cmake
+++ cmake/modules/FindMosquitto.cmake
@@ -6,15 +6,27 @@
 #  MOSQUITTO_LIBRARIES    - List of libraries when using libmosquitto.
 #  MOSQUITTO_FOUND        - True if libmosquitto found.
 
+# Debug output
+message(STATUS "FindMosquitto: CMAKE_PREFIX_PATH = ${CMAKE_PREFIX_PATH}")
+message(STATUS "FindMosquitto: VCPKG_ROOT = $ENV{VCPKG_ROOT}")
+message(STATUS "FindMosquitto: VCPKG_TARGET_TRIPLET = ${VCPKG_TARGET_TRIPLET}")
+message(STATUS "FindMosquitto: VCPKG_INSTALLED_DIR = ${VCPKG_INSTALLED_DIR}")
+
 if (NOT MOSQUITTO_INCLUDE_DIR)
   find_path(MOSQUITTO_INCLUDE_DIR mosquitto.h
-    PATHS
+    HINTS
       ${CMAKE_PREFIX_PATH}
+      ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}
+      $ENV{VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}
+      $ENV{VCPKG_ROOT}/installed/x64-windows
+    PATHS
       # macOS / Homebrew
       /opt/homebrew
       /usr/local
       /opt/homebrew/opt/mosquitto
       /usr/local/opt/mosquitto
+      # Linux
+      /usr
     PATH_SUFFIXES include
   )
 endif()
@@ -22,14 +34,20 @@ endif()
 if (NOT MOSQUITTO_LIBRARY)
   find_library(
     MOSQUITTO_LIBRARY
-    NAMES mosquitto
-    PATHS
+    NAMES mosquitto libmosquitto mosquittopp
+    HINTS
       ${CMAKE_PREFIX_PATH}
+      ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}
+      $ENV{VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}
+      $ENV{VCPKG_ROOT}/installed/x64-windows
+    PATHS
       # macOS / Homebrew
       /opt/homebrew
       /usr/local
       /opt/homebrew/opt/mosquitto
       /usr/local/opt/mosquitto
+      # Linux
+      /usr
     PATH_SUFFIXES lib
   )
 endif()
